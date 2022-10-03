8dbc04cfdbb957c420f51ad591d0c19059699635
James <zabaljames@gmailcom>
2022-10-03 14:50:05 +0800

    deleted nav

--- includes/nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="side-nav">
-    <img class="user-img" src="./assets/images/user-icon.png">
-    <ul class="side-menu">
-        <li><a href="index.php?page=dashboard">Dashboard</a></li>
-        <li><a href="index.php?page=Products">Products</a></li>
-        <li><a href="index.php?page=Sales">Sales</a></li>
-    </ul>
-</nav>
--- index.php
+++ index.php
@@ -3,14 +3,7 @@
 <body>
 
     <div class="container-fluid">
-        <div class="main-content">
-            <?php include('includes/nav.php')?>
-            <div class="contents dashboard-con">
-        
-
-            </div>
-        </div>      
+      
     </div>
-    
 </body>
 <?php include('includes/footer.php')?>
