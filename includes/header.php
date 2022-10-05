513bf4c1114a81a90556a663c852d3eeb627ee2e
James <zabaljames@gmailcom>
2022-10-05 09:34:11 +0800

    test

--- includes/header.php
+++ includes/header.php
@@ -9,10 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/style.css">
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100&display=swap" rel="stylesheet">
 </head>
 <header>
     <div class="main-menu-con">
+        <div class="my-logo">
+            <img src="./assets/images/socials/search.png" alt="">
+        </div>
         <ul class="main-menu">
             <li class="nav-item">Home</li>
             <li class="nav-item">Personal Information</li>
--- index.php
+++ index.php
@@ -5,6 +5,17 @@
     <div class="container-fluid">
       
         <div class="main-content">
+            <section class="main__banner">
+                <div class="main__banner_container">
+                    <div class="main_banner_first__col">
+                        <h1>James Gubaton</h1>
+                        <h6>Programmer</h6>
+                    </div>
+                    <div class="main_banner_second__col">
+                        <img src="./assets/images/__MACOS/svg/characters/Character03.svg" alt="">
+                    </div>
+                </div>  
+            </section>
         </div>      
     </div>
 </body>
